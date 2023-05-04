1ab109cec179a6b15380a7f7568bffd9a0eb4087
Julien Cristau <jcristau@mozilla.com>
2023-05-04 12:31:24 +0200

    No bug - fix release_started transform to not die if it can't find the previous version
    Makes it easier to run locally, just don't add the notification in that case.

--- taskcluster/android_taskgraph/transforms/release_started.py
+++ taskcluster/android_taskgraph/transforms/release_started.py
@@ -69,7 +69,8 @@ def add_github_link(config, tasks):
     repo = get_repository(".")
     git_tags = repo.run("tag", "-l", TAG_PREFIX + "*").splitlines()
     if not git_tags:
-        raise Exception("Git tags not found! Try running: git pull <remote> --tags")
+        yield from tasks
+        return
     previous_version = get_previous_tag_version(version, git_tags)
     current_revision = config.params["head_rev"]
     repo_url = config.params["base_repository"]
